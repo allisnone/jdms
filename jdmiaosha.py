0bf248470e52be2c0cce0c1dfe2fceba7082be7b
Administrator <Administrator@P-V-4>
2018-04-04 20:49:31 +0800

    uo

--- jdmiaosha.py
+++ jdmiaosha.py
@@ -13,7 +13,7 @@ def login(uname, pwd):
     driver.find_element_by_link_text("账户登录").click()
     driver.find_element_by_name("loginname").send_keys(uname)
     driver.find_element_by_name("nloginpwd").send_keys(pwd)
-    time.sleep(60)
+    time.sleep(30)
     driver.find_element_by_id("loginsubmit").click()
     time.sleep(3)
     driver.get("https://cart.jd.com/cart.action")
@@ -23,8 +23,16 @@ def login(uname, pwd):
     print(now.strftime('%Y-%m-%d %H:%M:%S'))
     print('login success')
 
-def miaosha_order(url='https://item.jd.com/19182651361.html'):
+def miaosha_order(good_id):
+    """
+    秒杀链接
+    """
+    #玩客云：https://item.jd.com/4993737.html
+    #https://item.jd.com/4993773.html
+    url='https://item.jd.com/%s.html'%(good_id)
     driver.get(url)
+    driver.find_element_by_id("btn-onkeybuy").click()
+    time.sleep(3)
     return
 # buytime = '2016-12-27 22:31:00' 
 def buy_on_time(buytime):
@@ -37,7 +45,8 @@ def buy_on_time(buytime):
             print('purchase success')
         time.sleep(0.5)
 
-
 # entrance
 login('usename', 'password')
+good_id = 6076915
+miaosha_order(good_id)
 buy_on_time('2017-01-01 14:00:00')
